595e8f97629b0b342f6cce06269f4b266a1caf09
Federico Ponzi <isaacisback92@gmail.com>
2020-07-08 20:27:02 +0200

    Adds solution to problem 5

---
+++ set-1/src/ex_5_repeating_key_xor.rs
@@ -0,0 +1,28 @@
+use std::ops::BitXor;
+
+fn repeating_xor_key(input: String, key: Vec<u8>) -> Vec<u8> {
+    input
+        .bytes()
+        .into_iter()
+        .zip(key.into_iter().cycle())
+        .map(|(ch, k)| ch.bitxor(k))
+        .collect()
+}
+
+#[cfg(test)]
+mod test {
+    use crate::ex_5_repeating_key_xor::repeating_xor_key;
+
+    #[test]
+    fn test_decipher() {
+        let expected = "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272"
+            .to_string()
+            + "a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f";
+        let input = r#"Burning 'em, if you ain't quick and nimble
+I go crazy when I hear a cymbal"#
+            .to_string();
+        let key = "ICE".bytes();
+        let received = repeating_xor_key(input, key.collect());
+        assert_eq!(hex::encode(received), expected);
+    }
+}
--- set-1/src/lib.rs
+++ set-1/src/lib.rs
@@ -7,4 +7,5 @@ mod ex_1_hex_to_b64;
 mod ex_2_fixed_xor;
 mod ex_3_single_byte_xor_cipher;
 mod ex_4_detect_single_character_xor;
+mod ex_5_repeating_key_xor;
 mod shared;

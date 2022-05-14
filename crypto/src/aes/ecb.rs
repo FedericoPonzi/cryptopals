66824a6f3abaa71e8273bf10d687d5415a402230
Federico Ponzi <me@fponzi.me>
2022-05-14 18:00:13 +0100

    Fixes wrong test in ecb encrypt

--- crypto/src/aes/ecb.rs
+++ crypto/src/aes/ecb.rs
@@ -50,11 +50,10 @@ where
 
 #[cfg(test)]
 mod test {
-    use crate::aes::ecb::decrypt;
     use crate::aes::ecb::encrypt;
     use crate::Pkcs7;
 
-    fn test_encrypt(plaintext: &[u8], expected_b64: String, key: &[u8; 16]) {
+    fn test_encrypt(plaintext: &[u8], expected_b64: &str, key: &[u8; 16]) {
         let padded = Pkcs7::pad(&plaintext.to_vec(), 16);
         let received = encrypt(key, &padded);
         let encoded = base64::encode(&received);
@@ -62,25 +61,26 @@ mod test {
     }
 
     #[test]
-    // Tested against:https://gchq.github.io/CyberChef/#recipe=AES_Encrypt(%7B'option':'Hex','string':'c4%2099%203c%2041%2031%203c%20f%2049%20c7%20dc%203f%2050%20dc%2069%20e5%209e'%7D,%7B'option':'Hex','string':''%7D,'ECB','Raw','Raw',%7B'option':'Hex','string':''%7D)To_Base64('A-Za-z0-9%2B/%3D')From_Base64('A-Za-z0-9%2B/%3D',true/disabled)AES_Decrypt(%7B'option':'Hex','string':'88%2065%2020%20a9%20b8%2086%20c6%20fe%2041%2018%2040%2050%2042%202f%207c%201a'%7D,%7B'option':'Hex','string':''%7D,'ECB','Raw','Raw',%7B'option':'Hex','string':''%7D,%7B'option':'Hex','string':''%7D/disabled)&input=VGhlIEFkdmFuY2VkIEVuY3J5cHRpb24gU3RhbmRhcmQgKEFFUyksIAphbHNvIGtub3duIGJ5IGl0cyBvcmlnaW5hbCBuYW1lIFJpam5kYWVsCmlzIGEgc3BlY2lmaWNhdGlvbiBmb3IgdGhlIGVuY3J5cHRpb24gb2YgZWxlY3Ryb25pYyBkYXRhIGVzdGFibGlzaGVkIApieSB0aGUgVS5TLiBOYXRpb25hbCBJbnN0aXR1dGUgb2YgU3RhbmRhcmRzIGFuZCBUZWNobm9sb2d5IChOSVNUKSBpbiAyMDAx
+    // Tested with:https://gchq.github.io/CyberChef/#recipe=AES_Encrypt(%7B'option':'Hex','string':'c4%2099%203c%2041%2031%203c%20f%2049%20c7%20dc%203f%2050%20dc%2069%20e5%209e'%7D,%7B'option':'Hex','string':''%7D,'ECB','Raw','Raw',%7B'option':'Hex','string':''%7D)To_Base64('A-Za-z0-9%2B/%3D')From_Base64('A-Za-z0-9%2B/%3D',true/disabled)AES_Decrypt(%7B'option':'Hex','string':'88%2065%2020%20a9%20b8%2086%20c6%20fe%2041%2018%2040%2050%2042%202f%207c%201a'%7D,%7B'option':'Hex','string':''%7D,'ECB','Raw','Raw',%7B'option':'Hex','string':''%7D,%7B'option':'Hex','string':''%7D/disabled)&input=VGhlIEFkdmFuY2VkIEVuY3J5cHRpb24gU3RhbmRhcmQgKEFFUyksIAphbHNvIGtub3duIGJ5IGl0cyBvcmlnaW5hbCBuYW1lIFJpam5kYWVsCmlzIGEgc3BlY2lmaWNhdGlvbiBmb3IgdGhlIGVuY3J5cHRpb24gb2YgZWxlY3Ryb25pYyBkYXRhIGVzdGFibGlzaGVkIApieSB0aGUgVS5TLiBOYXRpb25hbCBJbnN0aXR1dGUgb2YgU3RhbmRhcmRzIGFuZCBUZWNobm9sb2d5IChOSVNUKSBpbiAyMDAx
+    // beware of padding on cyberchef.
     fn test_ecb_encrypt() {
-        let plaintext = br#"The Advanced Encryption Standard (AES), 
+        let plaintext = br#"The Advanced Encryption Standard (AES) 
 also known by its original name Rijndael
 is a specification for the encryption of electronic data established 
 by the U.S. National Institute of Standards and Technology (NIST) in 2001"#;
 
-        // 88 65 20 a9 b8 86 c6 fe 41 18 40 50 42 2f 7c 1a
         let bad_key = &[
-            136, 101, 32, 169, 184, 134, 198, 254, 65, 24, 64, 80, 66, 47, 124, 26,
+            0x88, 0x65, 0x20, 0xa9, 0xb8, 0x86, 0xc6, 0xfe, 0x41, 0x18, 0x40, 0x50, 0x42, 0x2f,
+            0x7c, 0x1a,
         ];
-        let b_encr = "yL81OcwP6uL7ygcfNsKvK4PQH4F8tobUKGCSAmJ8psZCBLskIziEujjlLY46z3+PtoWWZoE9Zp2CtFbCUaUdWPgXtiZu5QoGcHLnZXnl1MT6VKgshgT1sVmUpXl2znTBJ2hkkGlB6Sa8reA7iGlomNkmcSvq1p8tW/lpk0ox5oK51zcEgNpPKxcqAUW9DEQQjKDnRaeunzPL4PfvT/KBAUGWWDhI/QxS5tuEO+AhOig8g578AEdbglLBOxaK5CZ8Mm3TWCa2IqMkrwfeZpMGnKxZn32z3liFhA9PgFlGSYs3u8ryDZQQk1u/A1oj8adu".to_string();
+        let b_encr = "yL81OcwP6uL7ygcfNsKvK4PQH4F8tobUKGCSAmJ8psZrzb7+9ZEztwrReNmUomWnGCfjDG4TVn5t7shxydEgOXDr2eGl8gD24nimLQj8Q42wesWJy73fll3nx6NhnTggTXMXB15UsWD3x8DNkjSc0HRTeK1sGbXUVBsZW3DlrtGlmNMYsiKqKvpWfKDMDR8angphgBdMamrS+EUTlOCo0eNnRx8AMGZqDWOwoiiu4dn3eQomR2cQqpT6XpB3cZgfY9POerfQpg3e2WMy+tICE1dFpBR0yLEz+W6N8D48EgvOHwjZtSitDuLECh5ATtCo";
         test_encrypt(plaintext, b_encr, bad_key);
 
-        // c4 99 3c 41 31 3c f 49 c7 dc 3f 50 dc 69 e5 9e
         let good_key = &[
-            196, 153, 60, 65, 49, 60, 15, 73, 199, 220, 63, 80, 220, 105, 229, 158,
+            0xc4, 0x99, 0x3c, 0x41, 0x31, 0x3c, 0x0f, 0x49, 0xc7, 0xdc, 0x3f, 0x50, 0xdc, 0x69,
+            0xe5, 0x9e,
         ];
-        let g_encr = "FglqVxpV4iYtdMkQgivE2adWYQVBJ5GScqDhvHR8jeWJLKCJRymUi/h2BjlKMOQQ/armPl2oIeZF17Yag5TF9jseZND8U88uOLnJTPLZAXVHcjIurgeFWokztrpNiA5XXuAVr1fKFrPLX5y6v836QdT9UMuKtrHmSHNWpqqACdrsVe5QPWdVTapRjaEnFZtArFeZkYkvxtzI9iH6143rc/fuCZlD9rZXMif9wW2iqORJ5RDKcmA8QU3oL0HXFP/xMlF2D3d6GvlMLloSedA0kYWOUhM+HCq9PLz5rumHiVEBvu+GA60CePW6sVWZhTmL".to_string();
+        let g_encr = "UUipw+CDhOAzlX1Wcw3aX2AkR069Vcw6x3BRsTleVtkNgvCgOO5mHayXjahZ8yx9q4qNXHFhIpk0+EMaKm/fdhwkyB5Y9BYC8FRk/oVgiQ/ISyHJA8e4WUqJvGl4kCGr8OHsui7WU6gg57a8rbc8jyIh3D5Yp+O8CY+wFPQ+LDAV1Im3TDxIb05U8WT23pQ70nGqS/BRfR58oeAwAqiDtqNQ1AnD9e/Y7qwEjRwIv/Eq/Sm/Nz3xZDDW7QU+ld/RsN7ktfTMMqa92LGGitDViP++X7sypzS7s0vP0HHxq3S3To54gGmRTR4cWcYz609a";
         test_encrypt(plaintext, g_encr, good_key);
     }
 }

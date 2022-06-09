282330895d2dff5d73ad1f88c2d5b1b960cd28e4
Federico Ponzi <me@fponzi.me>
2022-06-09 21:56:35 +0100

    Ex 22

--- crypto/src/lib.rs
+++ crypto/src/lib.rs
@@ -3,6 +3,7 @@ extern crate core;
 pub mod aes;
 mod cryptobreak;
 mod pkcs7;
+pub mod random;
 pub mod utils;
 
 pub use pkcs7::Pkcs7;
--- set_3/src/ex_22_crack_an_mt19937_seed.rs
+++ set_3/src/ex_22_crack_an_mt19937_seed.rs
@@ -16,3 +16,46 @@
 //!
 //! From the 32 bit RNG output, discover the seed.
 //!
+
+use crypto::random::Mt19937MersenneTwisterRng;
+use std::time::{SystemTime, UNIX_EPOCH};
+
+fn solve(random: u32) -> u32 {
+    let now = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .unwrap()
+        .as_secs() as u32;
+    for seed in now - 1 * 60..now {
+        let mut rng = Mt19937MersenneTwisterRng::new_seed(seed);
+        if rng.extract_number() == random {
+            return seed;
+        }
+    }
+    panic!("Seed not found!");
+}
+#[cfg(test)]
+mod test {
+    use crate::ex_22_crack_an_mt19937_seed::solve;
+    use crypto::random::Mt19937MersenneTwisterRng;
+    use rand::{random, Rng};
+    use std::time::{Duration, SystemTime, UNIX_EPOCH};
+
+    fn generate_seed() -> (u32, u32) {
+        let mut rng = rand::thread_rng();
+        std::thread::sleep(Duration::from_secs(rng.gen_range(4..10)));
+        let seed = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .unwrap()
+            .as_secs() as u32;
+        let mut rnd = Mt19937MersenneTwisterRng::new_seed(seed);
+        std::thread::sleep(Duration::from_secs(rng.gen_range(10..30)));
+        (rnd.extract_number(), seed)
+    }
+
+    #[test]
+    fn test_solve() {
+        let (random, expected) = generate_seed();
+        let received = solve(random);
+        assert_eq!(expected, received);
+    }
+}

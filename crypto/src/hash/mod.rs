1f634453a25cec2ac5ae3c7d781c735bf7b26c50
Federico Ponzi <me@fponzi.me>
2023-04-20 21:18:34 +0100

    solution to problem 31

--- crypto/src/hash/mod.rs
+++ crypto/src/hash/mod.rs
@@ -8,3 +8,22 @@ pub fn to_hex<T: AsRef<[u8]>>(input: T) -> String {
     }
     return s;
 }
+
+pub fn from_hex(hex: &str) -> Option<Vec<u8>> {
+    // Check that the input string has an even number of characters
+    if hex.len() % 2 != 0 {
+        return None;
+    }
+
+    // Convert each pair of hex digits to a byte
+    let mut bytes = Vec::new();
+    for i in 0..(hex.len() / 2) {
+        let byte = match u8::from_str_radix(&hex[i * 2..(i + 1) * 2], 16) {
+            Ok(byte) => byte,
+            Err(_) => return None,
+        };
+        bytes.push(byte);
+    }
+
+    Some(bytes)
+}
--- set_4/src/ex_31_implement_and_break_hmac_sha1_with_an_artificial_timing_leak.rs
+++ set_4/src/ex_31_implement_and_break_hmac_sha1_with_an_artificial_timing_leak.rs
@@ -31,35 +31,50 @@
 //! keeping things cryptographic in these challenges.
 
 use actix_web::{get, web, App, HttpResponse, HttpServer, ResponseError};
-use crypto::hash::to_hex;
+use crypto::hash::{from_hex, to_hex};
+use crypto::mac::hmac_sha1;
 use std::fmt::{Display, Formatter};
+use std::future::Future;
+use std::time::Instant;
 use std::{thread, time};
+
+const FILE: &[u8] =
+    b"Early-exit string compares are probably the most common source of cryptographic \
+timing leaks, but they aren't especially easy to exploit. In fact, many timing leaks (for instance\
+, any in C, C++, Ruby, or Python) probably aren't exploitable over a wide-area network at all. \
+To play with attacking real-world timing leaks, you have to start writing low-level timing code. \
+We're keeping things cryptographic in these challenges. ";
+
+const HMAC_KEY: &[u8] = b"some-secret-key";
+
+const LIMIT_FOR_TESTING: usize = 3;
 
 #[get("/test")]
 async fn verify_signature(
     query: web::Query<SignatureQuery>,
 ) -> Result<HttpResponse, SignatureError> {
-    let hmac_key = b"some-secret-key";
-    let mac = "";
-    let expected_signature = to_hex(mac.as_bytes());
-    if insecure_compare(&query.signature, expected_signature.as_bytes()) {
+    let mut mac = hmac_sha1(HMAC_KEY, &FILE);
+
+    // REMOVE ME: just for faster testing
+    mac.truncate(LIMIT_FOR_TESTING);
+    // -----
+
+    let expected_signature = &mac;
+    if insecure_compare(&from_hex(&query.signature).unwrap(), expected_signature) {
         Ok(HttpResponse::Ok().body("OK"))
     } else {
         Err(SignatureError::InvalidSignature)
     }
 }
 
-fn insecure_compare(a: &str, b: &[u8]) -> bool {
-    if a.len() != b.len() {
-        return false;
-    }
-    for (byte_a, byte_b) in a.bytes().zip(b.iter()) {
-        if byte_a != *byte_b {
+fn insecure_compare(a: &[u8], b: &[u8]) -> bool {
+    for (byte_a, byte_b) in a.into_iter().zip(b.iter()) {
+        if *byte_a != *byte_b {
             return false;
         }
         thread::sleep(time::Duration::from_millis(50));
     }
-    true
+    a.len() == b.len()
 }
 
 #[derive(serde::Deserialize)]
@@ -92,26 +107,72 @@ async fn main() -> std::io::Result<()> {
         .run()
         .await
 }
+use std::pin::Pin;
+
+async fn find_hmac<F>(sender: F) -> Vec<u8>
+where
+    F: Fn(Vec<u8>) -> Pin<Box<dyn Future<Output = bool>>>,
+{
+    let mut mac = hmac_sha1(HMAC_KEY, &FILE);
+    mac.truncate(LIMIT_FOR_TESTING);
+
+    let mut current_hmac = Vec::new();
+    loop {
+        let current_len = current_hmac.len();
+        for i in 0..=255u8 {
+            let mut buf = current_hmac.clone();
+            buf.push(i);
+            let start = Instant::now();
+            // Call your function here
+            let ret = sender(buf.clone()).await;
+            if ret {
+                if mac.len() != buf.len() {
+                    panic!("Something weird happened");
+                }
+                return buf;
+            }
+            let duration = start.elapsed();
+            if duration.as_millis() > (49 * buf.len()) as u128 {
+                current_hmac.push(i);
+                break;
+            }
+        }
+        if current_hmac.len() == current_len {
+            panic!(
+                "Failed to find next byte:{:?}, len: {}",
+                current_hmac, current_len
+            );
+        }
+    }
+}
 
 #[cfg(test)]
 mod tests {
-    use crate::ex_31_implement_and_break_hmac_sha1_with_an_artificial_timing_leak::verify_signature;
+    use crate::ex_31_implement_and_break_hmac_sha1_with_an_artificial_timing_leak::{
+        find_hmac, verify_signature, FILE, HMAC_KEY, LIMIT_FOR_TESTING,
+    };
     use actix_web::http::header::ContentType;
     use actix_web::{test, App};
+    use crypto::hash::to_hex;
+    use crypto::mac::hmac_sha1;
 
     #[actix_web::test]
     async fn test_index_get() {
-        let app = test::init_service(App::new().service(verify_signature)).await;
-        let req = test::TestRequest::default()
-            .uri("/test?file=foo&signature=46b4ec586117154dacd49d664e5d63fdc88efb51")
-            .insert_header(ContentType::plaintext())
-            .to_request();
-        println!("{:?}", req);
-        let resp = test::call_service(&app, req).await;
-        assert!(
-            resp.status().is_success(),
-            "Resp status was: {}",
-            resp.status()
-        );
+        let mut mac = hmac_sha1(HMAC_KEY, &FILE);
+        mac.truncate(LIMIT_FOR_TESTING);
+
+        let ret = find_hmac(move |buf| {
+            Box::pin(async move {
+                let app = test::init_service(App::new().service(verify_signature)).await;
+                let req = test::TestRequest::default()
+                    .uri(&format!("/test?file=foo&signature={}", to_hex(buf)))
+                    .insert_header(ContentType::plaintext())
+                    .to_request();
+                test::call_service(&app, req).await.status().is_success()
+            })
+        })
+        .await;
+
+        assert_eq!(ret, mac);
     }
 }
